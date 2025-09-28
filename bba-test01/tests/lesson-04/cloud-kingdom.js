b52e60436a5a47cc7121d7e606fac909b1f2ebdd
Hien <dothanhhien2409@gmail.com>
2025-09-28 20:44:37 +0700

    chored: fix convention name

--- bba-test01/tests/lesson-04/cloud-kingdom.js
+++ bba-test01/tests/lesson-04/cloud-kingdom.js
@@ -9,21 +9,21 @@
 
 let playerName = "Mario";
 let currentLives = 3;
-const COIN_LEVELS_1 = 25;
-const COIN_LEVELS_2 = 30;
-const COIN_LEVELS_3 = 45;
+const coinLevels_1 = 25;
+const coinLevels_2 = 30;
+const coinLevels_3 = 45;
 
 // a. Tính giá trị trung bình coin của 3 level và in ra console.
 
 // Cách 1: Tính trực tiếp trong console.log
-console.log("Giá trị trung bình coin của 3 level là", (`${((COIN_LEVELS_1 + COIN_LEVELS_2 + COIN_LEVELS_3) / 3)}`));
+console.log("Giá trị trung bình coin của 3 level là", (`${((coinLevels_1 + coinLevels_2 + coinLevels_3) / 3)}`));
 
 // Cách 2: Tính trước, rồi in ra
-const average = (COIN_LEVELS_1 + COIN_LEVELS_2 + COIN_LEVELS_3) / 3;
+const average = (coinLevels_1 + coinLevels_2 + coinLevels_3) / 3;
 console.log("Giá trị trung bình coin của 3 level là", `${average}`);
 
 // b. In ra số coin dư khi chia cho 3
 
-console.log("Số coin dư của level 1 khi chia cho 3 là", `${(COIN_LEVELS_1) % 3}`);
-console.log("Số coin dư của level 2 khi chia cho 3 là", `${(COIN_LEVELS_2) % 3}`);
-console.log("Số coin dư của level 3 khi chia cho 3 là", `${(COIN_LEVELS_3) % 3}`);
+console.log("Số coin dư của level 1 khi chia cho 3 là", `${(coinLevels_1) % 3}`);
+console.log("Số coin dư của level 2 khi chia cho 3 là", `${(coinLevels_2) % 3}`);
+console.log("Số coin dư của level 3 khi chia cho 3 là", `${(coinLevels_3) % 3}`);
